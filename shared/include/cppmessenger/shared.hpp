7d4dc9c4e80415d2b82528c796c3697dfe1b7830
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-14 22:31:18 +0200

    build: add shared library

--- shared/include/cppmessenger/shared.hpp
+++ shared/include/cppmessenger/shared.hpp
@@ -5,4 +5,4 @@ namespace cppmessenger {
 constexpr auto project_name = "cpp-messenger";
 constexpr auto project_version = "0.1.0";
 
-}; // namespace cppmessenger
+}
---
+++ shared/src/shared.cpp
@@ -0,0 +1,1 @@
+#include <cppmessenger/shared.hpp>
