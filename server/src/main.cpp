512026952529ab622bd123886785719f6af2fde0
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-14 22:31:49 +0200

    build: add server executable

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,1 +1,13 @@
-// CppMessenger server entry point.
+#include <iostream>
+
+#include <cppmessenger/shared.hpp>
+
+int main()
+{
+    std::cout << cppmessenger::project_name
+              << " server "
+              << cppmessenger::project_version
+              << '\n';
+
+    return 0;
+}
