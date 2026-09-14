d24b7f99292f793f087b1de148808760ba495c21
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-14 22:39:16 +0200

    build: configure compiler warnings

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,1 +1,13 @@
-# Compiler warnings configuration.
+function(cppmessenger_enable_warnings target)
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+        target_compile_options(${target} PRIVATE
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wconversion
+            -Wsign-conversion
+        )
+    elseif(MSVC)
+        target_compile_options(${target} PRIVATE /W4 /permissive-)
+    endif()
+endfunction()
