bef3bc3096635a00f317d0a088c2da91d64dc91f
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-15 17:37:09 +0200

    feat: add tcp client interface

---
+++ shared/include/cppmessenger/network/tcp_client.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+
+#include <cppmessenger/network/connection.hpp>
+
+namespace cppmessenger::network {
+
+class TcpClient final : public Connection {
+public:
+    TcpClient();
+    ~TcpClient() override;
+
+    TcpClient(const TcpClient&) = delete;
+    TcpClient& operator=(const TcpClient&) = delete;
+
+    TcpClient(TcpClient&&) noexcept;
+    TcpClient& operator=(TcpClient&&) noexcept;
+
+    bool connect(const Endpoint& endpoint) override;
+    void disconnect() noexcept override;
+
+    [[nodiscard]] bool is_connected() const noexcept override;
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+} // namespace cppmessenger::network
