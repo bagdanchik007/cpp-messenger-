c33201ab71cd05c774014247ea9a7a57112d7ac1
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-22 22:14:27 +0200

    feat(server): add message dispatcher

---
+++ server/src/message_dispatcher.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+#include <cppmessenger/server/message_handler.hpp>
+
+namespace cppmessenger::server {
+
+class MessageDispatcher {
+public:
+    using Handler = MessageHandler;
+
+    void register_handler(
+        std::string message_type,
+        Handler handler);
+
+private:
+    std::unordered_map<std::string, Handler> handlers_;
+};
+
+} // namespace cppmessenger::server
