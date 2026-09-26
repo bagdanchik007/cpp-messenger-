ff7ac7585091d89f24aa5a0231deb9d3a7790a6b
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-26 17:09:17 +0200

    refactor(server): separate message handler abstraction

--- server/include/cppmessenger/server/message_handler.hpp
+++ server/include/cppmessenger/server/message_handler.hpp
@@ -2,20 +2,9 @@
 
 #include <functional>
 #include <string>
-#include <unordered_map>
 
 namespace cppmessenger::server {
 
-class MessageDispatcher {
-public:
-    using Handler = std::function<std::string(const std::string&)>;
-
-    void register_handler(
-        std::string message_type,
-        Handler handler);
-
-private:
-    std::unordered_map<std::string, Handler> handlers_;
-};
+using MessageHandler = std::function<std::string(const std::string&)>;
 
 } // namespace cppmessenger::server
